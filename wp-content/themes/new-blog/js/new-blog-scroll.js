fed891efe1d7243159774551ac3f41c74c7849d1
xiaxixiang <Samary.xia@gmail.com>
2021-02-25 11:16:20 +0800

    add new theme

---
+++ wp-content/themes/new-blog/js/new-blog-scroll.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+    // // Handler for .ready() called.
+    jQuery('html, body').animate({
+        scrollTop: jQuery('#scroll-top').offset().top
+    }, 'slow');
+
+  });
+
