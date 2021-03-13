aabe92c82ee9e44553ee9df9e6d364f1abd20f8d
xiaxixiang <Samary.xia@gmail.com>
2021-03-13 18:08:09 +0800

    修改提交

---
+++ wp-content/plugins/rollback-update-failure/plugin.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Rollback Update Failure
+ *
+ * @package rollback-update-failure
+ * @author Andy Fragen <andy@thefragens.com>
+ * @license MIT
+ */
+
+/**
+ * Plugin Name: Rollback Update Failure
+ * Author: Andy Fragen
+ * Description: Feature plugin to test plugin/theme update failures and rollback to previous installed packages via zip/unzip.
+ * Version: 0.5.0
+ * Network: true
+ * License: MIT
+ * Text Domain: rollback-update-failure
+ * Requires PHP: 5.6
+ * Requires at least: 5.2
+ * GitHub Plugin URI: https://github.com/afragen/rollback-update-failure
+ * Primary Branch: main
+ */
+
+/**
+ * Class Rollback_Update_Failure.
+ *
+ * Feature plugin to test feasibility of using zip/unzip for plugin/theme update failures.
+ *
+ * These to be added to `wp-admin/includes/class-wp-upgrader.php`.
+ * WP_Upgrader::run()
+ */
+class Rollback_Update_Failure {
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// Zip the plugin/theme being updated to rollback directory.
+		add_filter( 'upgrader_pre_install', array( $this, 'zip_to_rollback_dir' ), 15, 2 );
+
+		// Extract zip rollback if install_package returns WP_Error.
+		add_filter( 'upgrader_install_package_result', array( $this, 'extract_rollback' ), 15, 2 );
+	}
+
+	/**
+	 * Create a zip archive of the plugin/theme being upgraded into a rollback directory.
+	 *
+	 * @since 5.x.0
+	 * @uses 'upgrader_pre_install' filter.
+	 *
+	 * @global WP_Filesystem_Base $wp_filesystem WordPress filesystem subclass.
+	 * @param bool  $response   Boolean response to 'upgrader_pre_install' filter.
+	 *                          Default is true.
+	 * @param array $hook_extra Array of data for plugin/theme being updated.
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function zip_to_rollback_dir( $response, $hook_extra ) {
+		global $wp_filesystem;
+
+		// Exit early on plugin/theme installation.
+		if ( isset( $hook_extra['action'] ) && 'install' === $hook_extra['action'] ) {
+			return $response;
+		}
+
+		// Setup variables.
+		if ( isset( $hook_extra['plugin'] ) ) {
+			$slug = dirname( $hook_extra['plugin'] );
+			$src  = WP_PLUGIN_DIR . '/' . $slug;
+		}
+		if ( isset( $hook_extra['theme'] ) ) {
+			$slug = $hook_extra['theme'];
+			$src  = get_theme_root() . '/' . $slug;
+		}
+		$rollback_dir = $wp_filesystem->wp_content_dir() . 'upgrade/rollback/';
+
+		// Zip can use a lot of memory. From `unzip_file()`.
+		wp_raise_memory_limit( 'admin' );
+
+		if ( $wp_filesystem->mkdir( $rollback_dir ) ) {
+			$path_prefix = strlen( $src ) + 1;
+			$zip         = new ZipArchive();
+
+			if ( true === $zip->open( "{$rollback_dir}{$slug}.zip", ZipArchive::CREATE | ZipArchive::OVERWRITE ) ) {
+				$files = new RecursiveIteratorIterator(
+					new RecursiveDirectoryIterator( $src ),
+					RecursiveIteratorIterator::LEAVES_ONLY
+				);
+
+				foreach ( $files as $file ) {
+					// Skip directories (they would be added automatically).
+					if ( ! $file->isDir() ) {
+						// Get real and relative path for current file.
+						$file_path     = $file->getRealPath();
+						$relative_path = substr( $file_path, $path_prefix );
+
+						// Add current file to archive.
+						$zip->addFile( $file_path, $relative_path );
+					}
+				}
+
+				$zip->close();
+			} else {
+				return new WP_Error( 'zip_rollback_failed', __( 'Zip plugin/theme to rollback directory failed.', 'rollback-update-failure' ) );
+			}
+		}
+
+		return $response;
+	}
+
+	/**
+	 * Extract zipped rollback to original location.
+	 *
+	 * @since 5.x.0
+	 * @uses 'upgrader_install_package_result' filter.
+	 *
+	 * @global WP_Filesystem_Base $wp_filesystem WordPress filesystem subclass.
+	 * @param bool|WP_Error $result     Result from `WP_Upgrader::install_package()`.
+	 * @param array         $hook_extra Array of data for plugin/theme being updated.
+	 *
+	 * @return bool|WP_Error
+	 */
+	public function extract_rollback( $result, $hook_extra ) {
+		global $wp_filesystem;
+
+		if ( apply_filters( 'rollback_update_testing', false ) && ! is_wp_error( $result ) ) {
+			$result = new WP_Error( 'test rollback', 'Just testing rollback' );
+		}
+
+		if ( ! is_wp_error( $result ) ) {
+			return $result;
+		}
+
+		// Exit early on plugin/theme installation.
+		if ( isset( $hook_extra['action'] ) && 'install' === $hook_extra['action'] ) {
+			return new WP_Error( 'extract_rollback_error', __( '$hook_extra set for installation', 'rollback-update-failure' ) );
+		}
+
+		// Setup variables.
+		$slug = false;
+		if ( isset( $hook_extra['plugin'] ) ) {
+			$type            = 'plugin';
+			$slug            = dirname( $hook_extra['plugin'] );
+			$destination_dir = "plugins/{$slug}";
+		}
+		if ( isset( $hook_extra['theme'] ) ) {
+			$type            = 'theme';
+			$slug            = $hook_extra['theme'];
+			$destination_dir = "themes/{$slug}";
+		}
+
+		if ( ! $slug ) {
+			return new WP_Error( 'extract_rollback_failed_slug', __( '$slug not identified.', 'rollback-update-failure' ) );
+		}
+		$destination  = $wp_filesystem->wp_content_dir() . trailingslashit( $destination_dir );
+		$rollback_dir = $wp_filesystem->wp_content_dir() . 'upgrade/rollback/';
+		$rollback     = $rollback_dir . "{$slug}.zip";
+
+		// Start with a clean slate.
+		if ( $wp_filesystem->is_dir( $destination ) ) {
+			$wp_filesystem->delete( $destination, true );
+		}
+
+		$unzip = unzip_file( $rollback, $destination );
+		if ( is_wp_error( $unzip ) ) {
+			/* translators: %1: plugin|theme, %2: plugin/theme slug */
+			return new WP_Error( 'extract_rollback_failed_unzip', sprintf( __( 'Rollback of %1$s %2$s failed.', 'rollback-update-failure' ), $type, $slug ) );
+		}
+
+		/* translators: %1: plugin|theme, %2: plugin/theme slug */
+		$success_message = sprintf( __( 'Rollback of %1$s %2$s succeeded.', 'rollback-update-failure' ), $type, $slug );
+
+		$error_code = $result->get_error_code();
+		$result->add( $error_code, $success_message );
+		$error_messages = $result->get_error_messages( $error_code );
+
+		// Recreate WP_Error messages.
+		unset( $result->errors[ $error_code ] );
+		$result->add( $error_code, implode( ', ', $error_messages ) );
+
+		return $result;
+	}
+}
+
+new Rollback_Update_Failure();
---
+++ wp-content/plugins/rollback-update-failure/readme.txt
@@ -0,0 +1,53 @@
+# Rollback Update Failure
+
+Plugin Name: Rollback Update Failure
+Contributors: afragen
+Tags: feature plugin, update, failure
+License: MIT
+Requires PHP: 5.6
+Requires at least: 5.2
+Tested up to: 5.7
+Stable Tag: 0.5.0
+
+Feature plugin for testing automatic rollback of a plugin or theme update failure.
+
+## Description
+
+This is a feature plugin based on the [PR](https://github.com/WordPress/wordpress-develop/pull/860) for [#51857](https://core.trac.wordpress.org/ticket/51857).
+
+The assumption is that most of the errors in large plugins/themes occur during the `copy_dir()` part of `WP_Upgrader::install_package()`. Trac ticket [#52342](https://core.trac.wordpress.org/ticket/52342) brought more error reporting to `copy_dir()` and Trac ticket [#52831](https://core.trac.wordpress.org/ticket/52381) provides a filter hook in order to process the rollback in the event of a plugin/theme update failure. As of WordPress 5.7-beta1 both of these tickets are in core.
+
+It is during the `WP_Upgrader::install_package()` that the currently installed plugin is deleted in anticipation of copying the new update into that location. Having an empty plugin/theme folder or an incompletely copied update seems to be the most common issue.
+
+There will be messaging in the event of an error and successful or unsuccessful rollback.
+
+## Testing
+
+There was much discussion regarding the thought that adding additional IO processes for the zip and unzip process could result in server timeout issues on resource starved shared hosts. Activating the feature plugin will result in the creation of a zipfile of the installed plugin/theme being updated.
+
+For the sake of testing assume any server timeout occurring during the update process might be releated to the additional IO processes creating the zipfile. Please report these in [GitHub Issues](https://github.com/afragen/rollback-update-failure/issues) and report your server details. ( Host, RAM, OS, etc. )
+
+To simulate a failure, use the filter `add_filter( 'rollback_update_testing', '__return_true' );`
+
+Alternatively you can install the [Rollback Update Testing](https://gist.github.com/afragen/80b68a6c8826ab37025b05d4519bb4bf) plugin, activating it as needed. If you have [GitHub Updater](https://github.com/afragen/github-updater) installed, you can easily install this **Gist** from the **Install Plugin** tab. Select **Gist** as the **Remote Repository Host**.
+
+## Updating
+
+Until this plugin is in the dot org plugin repository you can stay updated by installing  [GitHub Updater](https://github.com/afragen/github-updater)
+
+## Reporting
+
+Please submit [issues](https://github.com/afragen/rollback-update-failure/issues) and [PRs](https://github.com/afragen/rollback-update-failure/pulls) to GitHub.
+
+## Changelog
+
+Please see the Github repository: [CHANGELOG.md](https://github.com/afragen/rollback-update-failure/blob/main/CHANGELOG.md).
+
+#### 0.5.0 / 2021-02-10
+* initial commit
+* use simpler hook for `extract_rollback`
+* update for `upgrader_install_package_result` filter and parameters passed
+* add text domain
+* update error message display
+* added filter `rollback_update_testing` to simulate a failure.
+* override filter if there's already a WP_Error
