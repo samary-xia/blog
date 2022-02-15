5ccbf9971a308e2ae6e773ba715598d2839d9864
Luke <samary.xia@gmail.com>
2022-02-15 14:21:56 +0800

    code change

--- wp-admin/options-head.php
+++ wp-admin/options-head.php
@@ -8,11 +8,11 @@
  * @subpackage Administration
  */
 
-wp_reset_vars( array( 'action' ) );
+wp_reset_vars(array('action'));
 
-if ( isset( $_GET['updated'] ) && isset( $_GET['page'] ) ) {
-	// For back-compat with plugins that don't use the Settings API and just set updated=1 in the redirect.
-	add_settings_error( 'general', 'settings_updated', __( 'Settings saved.' ), 'success' );
+if (isset($_GET['updated']) && isset($_GET['page'])) {
+    // For back-compat with plugins that don't use the Settings API and just set updated=1 in the redirect.
+    add_settings_error('general', 'settings_updated', __('Settings saved.'), 'success');
 }
 
 settings_errors();
