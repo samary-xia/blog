d639d1c753f68071bd7691f0eda9e7d18e69821c
Luke <samary.xia@gmail.com>
2022-03-12 11:59:23 +0800

    code change

--- wp-admin/network/edit.php
+++ wp-admin/network/edit.php
@@ -8,13 +8,13 @@
  */
 
 /** Load WordPress Administration Bootstrap */
-require_once( dirname( __FILE__ ) . '/admin.php' );
+require_once(dirname(__FILE__) . '/admin.php');
 
-$action = ( isset( $_GET['action'] ) ) ? $_GET['action'] : '';
+$action = (isset($_GET['action'])) ? $_GET['action'] : '';
 
-if ( empty( $action ) ) {
-	wp_redirect( network_admin_url() );
-	exit;
+if (empty($action)) {
+    wp_redirect(network_admin_url());
+    exit;
 }
 
 /**
@@ -25,7 +25,7 @@ if ( empty( $action ) ) {
  *
  * @since 3.0.0
  */
-do_action( 'wpmuadminedit' );
+do_action('wpmuadminedit');
 
 /**
  * Fires the requested handler action.
@@ -35,7 +35,7 @@ do_action( 'wpmuadminedit' );
  *
  * @since 3.1.0
  */
-do_action( "network_admin_edit_{$action}" );
+do_action("network_admin_edit_{$action}");
 
-wp_redirect( network_admin_url() );
+wp_redirect(network_admin_url());
 exit();
