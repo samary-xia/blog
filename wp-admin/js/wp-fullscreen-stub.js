728fdca26177fe7be8d8eb55a3e808114dc0ac91
luke <xia.xixiang@shipper.id>
2022-09-21 09:37:42 +0800

    plugin update

--- wp-admin/js/wp-fullscreen-stub.js
+++ wp-admin/js/wp-fullscreen-stub.js
@@ -5,36 +5,37 @@
  * @removed 4.3.
  * @output wp-admin/js/wp-fullscreen-stub.js
  */
-( function() {
-	var noop = function(){};
+(function () {
+    var noop = function () {
+    };
 
-	window.wp = window.wp || {};
-	window.wp.editor = window.wp.editor || {};
-	window.wp.editor.fullscreen = {
-		bind_resize: noop,
-		dfwWidth: noop,
-		off: noop,
-		on: noop,
-		refreshButtons: noop,
-		resizeTextarea: noop,
-		save: noop,
-		switchmode: noop,
-		toggleUI: noop,
+    window.wp = window.wp || {};
+    window.wp.editor = window.wp.editor || {};
+    window.wp.editor.fullscreen = {
+        bind_resize: noop,
+        dfwWidth: noop,
+        off: noop,
+        on: noop,
+        refreshButtons: noop,
+        resizeTextarea: noop,
+        save: noop,
+        switchmode: noop,
+        toggleUI: noop,
 
-		settings: {},
-		pubsub: {
-			publish: noop,
-			subscribe: noop,
-			unsubscribe: noop,
-			topics: {}
-		},
-		fade: {
-			In: noop,
-			Out: noop
-		},
-		ui: {
-			fade: noop,
-			init: noop
-		}
-	};
+        settings: {},
+        pubsub: {
+            publish: noop,
+            subscribe: noop,
+            unsubscribe: noop,
+            topics: {}
+        },
+        fade: {
+            In: noop,
+            Out: noop
+        },
+        ui: {
+            fade: noop,
+            init: noop
+        }
+    };
 }());
