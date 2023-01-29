93c62363742c8f514a703b473eb2587a0c024a45
luke <xia.xixiang@shipper.id>
2023-01-29 15:01:57 +0800

    feat: rest-api hnage

--- wp-includes/Requests/Utility/FilteredIterator.php
+++ wp-includes/Requests/Utility/FilteredIterator.php
@@ -12,34 +12,37 @@
  * @package Requests
  * @subpackage Utilities
  */
-class Requests_Utility_FilteredIterator extends ArrayIterator {
-	/**
-	 * Callback to run as a filter
-	 *
-	 * @var callable
-	 */
-	protected $callback;
+class Requests_Utility_FilteredIterator extends ArrayIterator
+{
+    /**
+     * Callback to run as a filter
+     *
+     * @var callable
+     */
+    protected $callback;
 
-	/**
-	 * Create a new iterator
-	 *
-	 * @param array $data
-	 * @param callable $callback Callback to be called on each value
-	 */
-	public function __construct($data, $callback) {
-		parent::__construct($data);
+    /**
+     * Create a new iterator
+     *
+     * @param array $data
+     * @param callable $callback Callback to be called on each value
+     */
+    public function __construct($data, $callback)
+    {
+        parent::__construct($data);
 
-		$this->callback = $callback;
-	}
+        $this->callback = $callback;
+    }
 
-	/**
-	 * Get the current item's value after filtering
-	 *
-	 * @return string
-	 */
-	public function current() {
-		$value = parent::current();
-		$value = call_user_func($this->callback, $value);
-		return $value;
-	}
+    /**
+     * Get the current item's value after filtering
+     *
+     * @return string
+     */
+    public function current()
+    {
+        $value = parent::current();
+        $value = call_user_func($this->callback, $value);
+        return $value;
+    }
 }
--- wp-includes/rest-api/endpoints/class-wp-rest-blocks-controller.php
+++ wp-includes/rest-api/endpoints/class-wp-rest-blocks-controller.php
@@ -17,77 +17,81 @@
  * @see WP_REST_Posts_Controller
  * @see WP_REST_Controller
  */
-class WP_REST_Blocks_Controller extends WP_REST_Posts_Controller {
+class WP_REST_Blocks_Controller extends WP_REST_Posts_Controller
+{
 
-	/**
-	 * Checks if a block can be read.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param object $post Post object that backs the block.
-	 * @return bool Whether the block can be read.
-	 */
-	public function check_read_permission( $post ) {
-		// Ensure that the user is logged in and has the read_blocks capability.
-		$post_type = get_post_type_object( $post->post_type );
-		if ( ! current_user_can( $post_type->cap->read_post, $post->ID ) ) {
-			return false;
-		}
+    /**
+     * Checks if a block can be read.
+     *
+     * @param object $post Post object that backs the block.
+     * @return bool Whether the block can be read.
+     * @since 5.0.0
+     *
+     */
+    public function check_read_permission($post)
+    {
+        // Ensure that the user is logged in and has the read_blocks capability.
+        $post_type = get_post_type_object($post->post_type);
+        if (!current_user_can($post_type->cap->read_post, $post->ID)) {
+            return false;
+        }
 
-		return parent::check_read_permission( $post );
-	}
+        return parent::check_read_permission($post);
+    }
 
-	/**
-	 * Filters a response based on the context defined in the schema.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @param array  $data    Response data to fiter.
-	 * @param string $context Context defined in the schema.
-	 * @return array Filtered response.
-	 */
-	public function filter_response_by_context( $data, $context ) {
-		$data = parent::filter_response_by_context( $data, $context );
+    /**
+     * Filters a response based on the context defined in the schema.
+     *
+     * @param array $data Response data to fiter.
+     * @param string $context Context defined in the schema.
+     * @return array Filtered response.
+     * @since 5.0.0
+     *
+     */
+    public function filter_response_by_context($data, $context)
+    {
+        $data = parent::filter_response_by_context($data, $context);
 
-		/*
-		 * Remove `title.rendered` and `content.rendered` from the response. It
-		 * doesn't make sense for a reusable block to have rendered content on its
-		 * own, since rendering a block requires it to be inside a post or a page.
-		 */
-		unset( $data['title']['rendered'] );
-		unset( $data['content']['rendered'] );
+        /*
+         * Remove `title.rendered` and `content.rendered` from the response. It
+         * doesn't make sense for a reusable block to have rendered content on its
+         * own, since rendering a block requires it to be inside a post or a page.
+         */
+        unset($data['title']['rendered']);
+        unset($data['content']['rendered']);
 
-		return $data;
-	}
+        return $data;
+    }
 
-	/**
-	 * Retrieves the block's schema, conforming to JSON Schema.
-	 *
-	 * @since 5.0.0
-	 *
-	 * @return array Item schema data.
-	 */
-	public function get_item_schema() {
-		// Do not cache this schema because all properties are derived from parent controller.
-		$schema = parent::get_item_schema();
+    /**
+     * Retrieves the block's schema, conforming to JSON Schema.
+     *
+     * @return array Item schema data.
+     * @since 5.0.0
+     *
+     */
+    public function get_item_schema()
+    {
+        // Do not cache this schema because all properties are derived from parent controller.
+        $schema = parent::get_item_schema();
 
-		/*
-		 * Allow all contexts to access `title.raw` and `content.raw`. Clients always
-		 * need the raw markup of a reusable block to do anything useful, e.g. parse
-		 * it or display it in an editor.
-		 */
-		$schema['properties']['title']['properties']['raw']['context']   = array( 'view', 'edit' );
-		$schema['properties']['content']['properties']['raw']['context'] = array( 'view', 'edit' );
+        /*
+         * Allow all contexts to access `title.raw` and `content.raw`. Clients always
+         * need the raw markup of a reusable block to do anything useful, e.g. parse
+         * it or display it in an editor.
+         */
+        $schema['properties']['title']['properties']['raw']['context'] = array('view', 'edit');
+        $schema['properties']['content']['properties']['raw']['context'] = array('view', 'edit');
 
-		/*
-		 * Remove `title.rendered` and `content.rendered` from the schema. It doesn’t
-		 * make sense for a reusable block to have rendered content on its own, since
-		 * rendering a block requires it to be inside a post or a page.
-		 */
-		unset( $schema['properties']['title']['properties']['rendered'] );
-		unset( $schema['properties']['content']['properties']['rendered'] );
+        /*
+         * Remove `title.rendered` and `content.rendered` from the schema. It doesn’t
+         * make sense for a reusable block to have rendered content on its own, since
+         * rendering a block requires it to be inside a post or a page.
+         */
+        unset($schema['properties']['title']['properties']['rendered']);
+        unset($schema['properties']['content']['properties']['rendered']);
 
-		return $schema;
-	}
+        return $schema;
+    }
 
 }
